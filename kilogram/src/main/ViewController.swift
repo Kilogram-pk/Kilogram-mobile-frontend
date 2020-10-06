8617603377a6fda820776b3603e300d03c713dd7
Huzaifa Khan <hak@embrace-it.com>
2020-10-06 14:26:21 +0500

    Chores: got user info through graphapi

--- kilogram/src/main/ViewController.swift
+++ kilogram/src/main/ViewController.swift
@@ -10,7 +10,7 @@ import UIKit
 import FBSDKLoginKit
 
 class ViewController: UIViewController {
-
+    
     var email: String = "";
     var password: String = "";
     
@@ -34,45 +34,56 @@ class ViewController: UIViewController {
     
     func createBottomText() {
         let attrs1 = [ NSAttributedString.Key.foregroundColor : UIColor.black,
-                      NSAttributedString.Key.font: UIFont(name: "HelveticaNeue", size: 12.0)!]
-
+                       NSAttributedString.Key.font: UIFont(name: "HelveticaNeue", size: 12.0)!]
+        
         let attrs2 = [NSAttributedString.Key.foregroundColor : UIColor.lightGray,
-        NSAttributedString.Key.font: UIFont(name: "HelveticaNeue", size: 12.0)!]
-
+                      NSAttributedString.Key.font: UIFont(name: "HelveticaNeue", size: 12.0)!]
+        
         let attributedString1 = NSMutableAttributedString(string:" Sign Up", attributes:attrs1)
-
+        
         let attributedString2 = NSMutableAttributedString(string:"Don't have an account?", attributes:attrs2)
         attributedString2.append(attributedString1);
         signUpButton.setAttributedTitle(attributedString2, for: .normal)
         
-//        bottomText.attributedText = attributedString2
+        //        bottomText.attributedText = attributedString2
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        let loginButton = FBLoginButton()
-        loginButton.center = view.center
-        view.addSubview(loginButton)
+        
+        let fbLogin = FBLoginButton()
+        fbLogin.center = CGPoint(x: UIScreen.main.bounds.width / 2 , y: UIScreen.main.bounds.height / 1.6)
+        fbLogin.permissions = ["public_profile", "email"]
+        fbLogin.delegate = self
+        view.addSubview(fbLogin)
         createBottomText()
         loginButton.layer.cornerRadius = 5
-//        let screenSize: CGRect = UIScreen.main.bounds // get screen bounds
-//
-//        let title = UILabel()
-//        title.text = "Some Sentence"
-//        title.numberOfLines = 0
-//        title.textAlignment = .center
-//        title.sizeToFit()
-//        title.backgroundColor = UIColor.red
-//        self.view.addSubview(title)
-//
-//        let label = UILabel(frame: CGRect(x: 0, y: 0, width: 200, height: 21))
-//        label.center = CGPoint(x: 160, y: 285)
-//        label.textAlignment = .center
-//        label.text = "I'm a test label"
-//        self.view.addSubview(label)
+        
+        if let token = AccessToken.current, !token.isExpired {
+            print("User already logged in ", AccessToken.current!)
+            let request = GraphRequest(graphPath: "me", parameters: ["fields": "id, email, first_name, last_name"], tokenString: AccessToken.current?.tokenString, version: Settings.defaultGraphAPIVersion, httpMethod: HTTPMethod.get)
+                   request.start { (connection, result, error) in
+                       if let err = error {
+                           print("Facebook graph request error: \(err)")
+                       } else {
+                           print("Facebook graph request successful!")
 
-
-        // Do any additional setup after loading the view.
+                           guard let json = result as? NSDictionary else { return }
+                           if let email = json["email"] as? String {
+                               print("\(email)")
+                           }
+                           if let firstName = json["first_name"] as? String {
+                               print("\(firstName)")
+                           }
+                           if let lastName = json["last_name"] as? String {
+                               print("\(lastName)")
+                           }
+                           if let id = json["id"] as? String {
+                               print("\(id)")
+                           }
+                       }
+                   }
+        }
     }
     
     func isLoginAvailable() {
@@ -83,24 +94,35 @@ class ViewController: UIViewController {
             loginButton.backgroundColor = UIColor(rgb: 0x000000)
         }
     }
-
+    
 }
 
 
 extension UIColor {
-   convenience init(red: Int, green: Int, blue: Int) {
-       assert(red >= 0 && red <= 255, "Invalid red component")
-       assert(green >= 0 && green <= 255, "Invalid green component")
-       assert(blue >= 0 && blue <= 255, "Invalid blue component")
-
-       self.init(red: CGFloat(red) / 255.0, green: CGFloat(green) / 255.0, blue: CGFloat(blue) / 255.0, alpha: 1.0)
-   }
-
-   convenience init(rgb: Int) {
-       self.init(
-           red: (rgb >> 16) & 0xFF,
-           green: (rgb >> 8) & 0xFF,
-           blue: rgb & 0xFF
-       )
-   }
+    convenience init(red: Int, green: Int, blue: Int) {
+        assert(red >= 0 && red <= 255, "Invalid red component")
+        assert(green >= 0 && green <= 255, "Invalid green component")
+        assert(blue >= 0 && blue <= 255, "Invalid blue component")
+        
+        self.init(red: CGFloat(red) / 255.0, green: CGFloat(green) / 255.0, blue: CGFloat(blue) / 255.0, alpha: 1.0)
+    }
+    
+    convenience init(rgb: Int) {
+        self.init(
+            red: (rgb >> 16) & 0xFF,
+            green: (rgb >> 8) & 0xFF,
+            blue: rgb & 0xFF
+        )
+    }
 }
+
+extension ViewController: LoginButtonDelegate {
+    func loginButtonDidLogOut(_ loginButton: FBLoginButton) {
+        print("logout")
+    }
+    
+    func loginButton(_ loginButton: FBLoginButton, didCompleteWith result: LoginManagerLoginResult?, error: Error?) {
+        print("Login button pressed")
+    }
+}
+
