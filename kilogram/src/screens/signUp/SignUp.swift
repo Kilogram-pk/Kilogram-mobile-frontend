90ef9f44cee6195af83232fdfd32091381c6050f
Huzaifa Khan <hak@embrace-it.com>
2020-10-22 11:38:20 +0500

    Feat: signup validation added

--- kilogram/src/screens/signUp/SignUp.swift
+++ kilogram/src/screens/signUp/SignUp.swift
@@ -9,11 +9,36 @@
 import UIKit
 
 class SignUp: UIViewController {
-    @IBOutlet weak var textField: TextFieldController!
+    
+    var phoneNumber: NSString = "";
+    
+    @IBOutlet weak var errorLabel: UILabel!
     @IBOutlet var button: UIView!
+    @IBOutlet var validationHeight: NSLayoutConstraint!
+    @IBOutlet weak var buttonTopConstant: NSLayoutConstraint!
+    
+    @IBAction func onPhoneChanged(_ sender: UITextField) {
+        phoneNumber = sender.text as! NSString
+    }
+    
+    
+    @IBAction func onButtonPress(_ sender: UIButton) {
+        print("****pH", phoneNumber)
+        if(phoneNumber.length < 5){
+            errorLabel.text = "Looks like your phone number maybe incorrect.\nPlease try entering your full number, including the country code."
+            errorLabel.isHidden = false
+            errorLabel.textColor = UIColor(red: 255, green: 0, blue: 0, alpha: 0.7)
+            errorLabel.font = UIFont(name: errorLabel.font.fontName, size: 14)
+//            validationHeight.constant = 30
+            buttonTopConstant.constant = 18
+        }
+    }
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
-       
+        validationHeight.constant = 0
+        errorLabel.font = UIFont(name: "SanFranciscoText-Medium", size: 0)
     }
+
 }
